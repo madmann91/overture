fc4b0aab0c3d0975595f811fa32da29114dbffc8
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2024-04-08 10:45:55 +0200

    Use pthreads instead of C11 threads for thread pool

--- src/overture/thread_pool.c
+++ src/overture/thread_pool.c
@@ -2,7 +2,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
-#include <threads.h>
+
+#include <pthread.h>
 
 #include "thread_pool.h"
 #include "alloc.h"
@@ -16,8 +17,8 @@ struct work_queue {
     size_t done_count;            // The number of items that are finished
     size_t done_target;           // The number of items that are required before the next synchronization
     size_t worked_on;             // The number of items being worked on
-    cnd_t avail_cond, done_cond;
-    mtx_t mutex;
+    pthread_cond_t avail_cond, done_cond;
+    pthread_mutex_t mutex;
 };
 
 struct thread_data {
@@ -27,7 +28,7 @@ struct thread_data {
 };
 
 struct thread_pool {
-    thrd_t* threads;
+    pthread_t* threads;
     size_t thread_count;
     bool should_stop;
     struct work_queue queue;
@@ -58,15 +59,15 @@ static inline bool waiting_condition(const struct work_queue* queue) {
         (queue->done_target == 0 || queue->done_count < queue->done_target);
 }
 
-static int thread_pool_worker(void* data) {
+static void* thread_pool_worker(void* data) {
     struct thread_data* thread_data = data;
     struct work_queue* queue = thread_data->queue;
     struct thread_pool* thread_pool = thread_data->thread_pool;
     size_t thread_id = thread_data->thread_id;
     while (true) {
-        mtx_lock(&queue->mutex);
+        pthread_mutex_lock(&queue->mutex);
         while (!queue->first_item) {
-            if (thread_pool->should_stop || cnd_wait(&queue->avail_cond, &queue->mutex) != thrd_success)
+            if (thread_pool->should_stop || pthread_cond_wait(&queue->avail_cond, &queue->mutex) != 0)
                 goto end;
         }
         struct work_item* item = queue->first_item;
@@ -76,30 +77,30 @@ static int thread_pool_worker(void* data) {
             queue->last_item = NULL;
         }
         queue->worked_on++;
-        mtx_unlock(&queue->mutex);
+        pthread_mutex_unlock(&queue->mutex);
 
         item->work_fn(item, thread_id);
 
-        mtx_lock(&queue->mutex);
+        pthread_mutex_lock(&queue->mutex);
         item->next = queue->done_items;
         queue->done_items = item;
         queue->worked_on--;
         queue->done_count++;
         if (!waiting_condition(queue))
-            cnd_signal(&queue->done_cond);
-        mtx_unlock(&queue->mutex);
+            pthread_cond_signal(&queue->done_cond);
+        pthread_mutex_unlock(&queue->mutex);
     }
 end:
-    mtx_unlock(&queue->mutex);
-    return 0;
+    pthread_mutex_unlock(&queue->mutex);
+    return NULL;
 }
 
 static inline bool init_work_queue(struct work_queue* queue) {
-    if (cnd_init(&queue->avail_cond) != thrd_success)
+    if (pthread_cond_init(&queue->avail_cond, NULL) != 0)
         return false;        
-    if (cnd_init(&queue->done_cond) != thrd_success)
+    if (pthread_cond_init(&queue->done_cond, NULL) != 0)
         goto cleanup_cond;
-    if (mtx_init(&queue->mutex, mtx_plain) != thrd_success)
+    if (pthread_mutex_init(&queue->mutex, NULL) != 0)
         goto cleanup_mutex;
     queue->first_item = NULL;
     queue->last_item = NULL;
@@ -109,25 +110,25 @@ static inline bool init_work_queue(struct work_queue* queue) {
     queue->done_target = 0;
     return true;
 cleanup_mutex:
-    cnd_destroy(&queue->done_cond);
+    pthread_cond_destroy(&queue->done_cond);
 cleanup_cond:
-    cnd_destroy(&queue->avail_cond);
+    pthread_cond_destroy(&queue->avail_cond);
     return false;
 }
 
 static inline void free_work_queue(struct work_queue* queue) {
-    mtx_destroy(&queue->mutex);
-    cnd_destroy(&queue->avail_cond);
-    cnd_destroy(&queue->done_cond);
+    pthread_mutex_destroy(&queue->mutex);
+    pthread_cond_destroy(&queue->avail_cond);
+    pthread_cond_destroy(&queue->done_cond);
 }
 
 static inline void terminate_threads(struct thread_pool* thread_pool) {
-    mtx_lock(&thread_pool->queue.mutex);
+    pthread_mutex_lock(&thread_pool->queue.mutex);
     thread_pool->should_stop = true;
-    cnd_broadcast(&thread_pool->queue.avail_cond);
-    mtx_unlock(&thread_pool->queue.mutex);
+    pthread_cond_broadcast(&thread_pool->queue.avail_cond);
+    pthread_mutex_unlock(&thread_pool->queue.mutex);
     for (size_t i = 0, n = thread_pool->thread_count; i < n; ++i)
-        thrd_join(thread_pool->threads[i], NULL);
+        pthread_join(thread_pool->threads[i], NULL);
 }
 
 struct thread_pool* thread_pool_create(size_t thread_count) {
@@ -137,14 +138,14 @@ struct thread_pool* thread_pool_create(size_t thread_count) {
     if (!init_work_queue(&thread_pool->queue))
         goto cleanup_queue;
     thread_pool->thread_data = xmalloc(sizeof(struct thread_data) * thread_count);
-    thread_pool->threads = xmalloc(sizeof(thrd_t) * thread_count);
+    thread_pool->threads = xmalloc(sizeof(pthread_t) * thread_count);
     thread_pool->thread_count = thread_count;
     thread_pool->should_stop = false;
     for (size_t i = 0; i < thread_count; ++i) {
         thread_pool->thread_data[i].thread_pool = thread_pool;
         thread_pool->thread_data[i].queue = &thread_pool->queue;
         thread_pool->thread_data[i].thread_id = i;
-        if (thrd_create(thread_pool->threads + i, thread_pool_worker, &thread_pool->thread_data[i]) != thrd_success) {
+        if (pthread_create(thread_pool->threads + i, NULL, thread_pool_worker, &thread_pool->thread_data[i]) != 0) {
             thread_pool->thread_count = i;
             goto cleanup_thread;
         }
@@ -179,7 +180,7 @@ void thread_pool_submit(struct thread_pool* thread_pool, struct work_item* first
     while (prev->next) prev = prev->next;
     assert(prev == last);
 #endif
-    mtx_lock(&thread_pool->queue.mutex);
+    pthread_mutex_lock(&thread_pool->queue.mutex);
     if (thread_pool->queue.last_item) {
         assert(thread_pool->queue.first_item);
         thread_pool->queue.last_item->next = first;
@@ -190,20 +191,19 @@ void thread_pool_submit(struct thread_pool* thread_pool, struct work_item* first
         thread_pool->queue.last_item  = last;
     }
     if (first == last)
-        cnd_signal(&thread_pool->queue.avail_cond);
+        pthread_cond_signal(&thread_pool->queue.avail_cond);
     else
-        cnd_broadcast(&thread_pool->queue.avail_cond);
-    mtx_unlock(&thread_pool->queue.mutex);
+        pthread_cond_broadcast(&thread_pool->queue.avail_cond);
+    pthread_mutex_unlock(&thread_pool->queue.mutex);
 }
 
 struct work_item* thread_pool_wait(struct thread_pool* thread_pool, size_t count) {
     struct work_queue* queue = &thread_pool->queue;
     struct work_item* done_items = NULL;
-    mtx_lock(&queue->mutex);
+    pthread_mutex_lock(&queue->mutex);
     queue->done_target = count;
     while (true) {
-        if (waiting_condition(queue) &&
-            cnd_wait(&queue->done_cond, &queue->mutex) == thrd_success)
+        if (waiting_condition(queue) && pthread_cond_wait(&queue->done_cond, &queue->mutex) == 0)
             continue;
         break;
     }
@@ -211,6 +211,6 @@ struct work_item* thread_pool_wait(struct thread_pool* thread_pool, size_t count
     queue->done_items = NULL;
     queue->done_count = 0;
     queue->done_target = 0;
-    mtx_unlock(&queue->mutex);
+    pthread_mutex_unlock(&queue->mutex);
     return done_items;
 }
