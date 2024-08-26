462c9334c18e4b1f58fbd715174f710e75d60729
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2024-08-26 20:59:08 +0200

    Add array allocation macro for memory pool

--- src/overture/mem_pool.h
+++ src/overture/mem_pool.h
@@ -19,6 +19,9 @@ struct mem_pool {
 /// Allocates memory for an object of the given type.
 #define MEM_POOL_ALLOC(pool, T) mem_pool_alloc(&(pool), sizeof(T), alignof(T))
 
+/// Allocates memory for an object of the given type.
+#define MEM_POOL_ALLOC_ARRAY(pool, n, T) mem_pool_alloc(&(pool), sizeof(T) * n, alignof(T))
+
 /// Creates an empty memory pool.
 [[nodiscard]] struct mem_pool mem_pool_create(void);
 
@@ -29,7 +32,7 @@ void mem_pool_destroy(struct mem_pool*);
 /// This does not free the memory, but instead makes it available for the following allocations.
 void mem_pool_reset(struct mem_pool*);
 
-/// Allocates a memory on a pool.
+/// Allocates memory on a pool.
 /// @param mem_pool Memory pool to use.
 /// @param size Size of the object to allocate (in bytes).
 /// @param align Alignment of the object to allocate (in bytes).
