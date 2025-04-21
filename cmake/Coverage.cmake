0f804222cc2e6ac584186f761e56b1f8a9bcb95b
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2025-04-21 15:47:16 +0200

    Cleanup coverage generation logic

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -2,20 +2,18 @@ if (ENABLE_COVERAGE)
     find_program(GCOVR_COMMAND gcovr REQUIRED)
     if (GCOVR_COMMAND)
         # Create a "coverage" target
-        set(CMAKE_CXX_FLAGS_COVERAGE
-            "-g -fprofile-arcs -ftest-coverage"
+        set(CMAKE_CXX_FLAGS_COVERAGE "-g -fprofile-arcs -ftest-coverage"
             CACHE STRING "Flags used by the C++ compiler during coverage builds."
             FORCE)
-        set(CMAKE_C_FLAGS_COVERAGE
-            "-g -fprofile-arcs -ftest-coverage"
+        set(CMAKE_C_FLAGS_COVERAGE "-g -fprofile-arcs -ftest-coverage"
             CACHE STRING "Flags used by the C++ compiler during coverage builds."
             FORCE)
         set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG}"
             CACHE STRING "Flags used for linking binaries during coverage builds."
-            FORCE )
+            FORCE)
         set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}"
             CACHE STRING "Flags used by the shared libraries linker during coverage builds."
-            FORCE )
+            FORCE)
         mark_as_advanced(
             CMAKE_CXX_FLAGS_COVERAGE
             CMAKE_C_FLAGS_COVERAGE
@@ -23,7 +21,7 @@ if (ENABLE_COVERAGE)
             CMAKE_SHARED_LINKER_FLAGS_COVERAGE)
         add_custom_target(coverage
             COMMENT "Generating coverage report"
-            COMMAND ${GCOVR_COMMAND} -r ${PROJECT_SOURCE_DIR} --html coverage.html
+            COMMAND ${GCOVR_COMMAND} -r ${PROJECT_SOURCE_DIR} --html-details coverage.html .
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
     else()
         message(WARNING "Cannot find gcovr. Coverage target cannot be created.")
