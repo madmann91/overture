7d029f70b74c5caa794af9b558b786837c52f98e
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2025-04-28 17:38:19 +0200

    Change coverage options

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -2,16 +2,16 @@ if (ENABLE_COVERAGE)
     find_program(GCOVR_COMMAND gcovr REQUIRED)
     if (GCOVR_COMMAND)
         # Create a "coverage" target
-        set(CMAKE_CXX_FLAGS_COVERAGE "-g -fprofile-arcs -ftest-coverage"
+        set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_DEBUG} --coverage"
             CACHE STRING "Flags used by the C++ compiler during coverage builds."
             FORCE)
-        set(CMAKE_C_FLAGS_COVERAGE "-g -fprofile-arcs -ftest-coverage"
+        set(CMAKE_C_FLAGS_COVERAGE "${CMAKE_C_FLAGS_DEBUG} --coverage"
             CACHE STRING "Flags used by the C++ compiler during coverage builds."
             FORCE)
-        set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG}"
+        set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${CMAKE_EXE_LINKER_FLAGS_DEBUG} --coverage"
             CACHE STRING "Flags used for linking binaries during coverage builds."
             FORCE)
-        set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG}"
+        set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} --coverage"
             CACHE STRING "Flags used by the shared libraries linker during coverage builds."
             FORCE)
         mark_as_advanced(
